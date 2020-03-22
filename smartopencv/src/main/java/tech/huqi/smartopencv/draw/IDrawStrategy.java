93f37c3687f462040f007158953b057172cab677
hq_sunshine <hq_sunshine@163.com>
2020-03-22 16:56:56 +0800

    :art: style: 更新接口注释说明

--- smartopencv/src/main/java/tech/huqi/smartopencv/draw/IDrawStrategy.java
+++ smartopencv/src/main/java/tech/huqi/smartopencv/draw/IDrawStrategy.java
@@ -3,6 +3,8 @@ package tech.huqi.smartopencv.draw;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+import java.util.List;
+
 /**
  * Created by hzhuqi on 2019/9/3
  */
@@ -16,7 +18,9 @@ public interface IDrawStrategy {
      * 绘制每一帧图像
      *
      * @param canvas         画布
-     * @param frameBitmap    待绘制图像帧的{@code Bitmap}对象
+     * @param frameBitmap    待绘制图像帧的{@code Bitmap}对象，其宽高分别等同于预览帧大小计算接口
+     *                       {@link IPreviewSizeCalculator#calculateCameraFrameSize(List, int, int)}
+     *                       返回的Size宽和高
      * @param surfaceWidth   SurfaceView的宽度
      * @param surfaceHeight  SurfaceView的高度
      * @param isSetLandscape 是否设置为横屏
--- smartopencv/src/main/java/tech/huqi/smartopencv/draw/IPreviewSizeCalculator.java
+++ smartopencv/src/main/java/tech/huqi/smartopencv/draw/IPreviewSizeCalculator.java
@@ -12,7 +12,7 @@ public interface IPreviewSizeCalculator {
      * @param supportedSizes 当前设备摄像头所支持的预览大小列表
      * @param surfaceWidth   预览控件的宽度
      * @param surfaceHeight  预览控件的高度
-     * @return
+     * @return 设置给摄像头的预览宽高Size
      */
     Size calculateCameraFrameSize(List<Size> supportedSizes, int surfaceWidth, int surfaceHeight);
 }
