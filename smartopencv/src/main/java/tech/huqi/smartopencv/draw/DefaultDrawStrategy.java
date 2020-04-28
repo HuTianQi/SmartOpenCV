fc765f1d4d6d07a9cdc62ceb0b2b44044f4787ba
hq_sunshine <hq_sunshine@163.com>
2020-04-28 20:34:21 +0800

    :zap: refactor：优化默认的预览绘制逻辑，当预览分辨率与控件大小存在差异时将预览尽可能绘制在正中央

--- smartopencv/src/main/java/tech/huqi/smartopencv/draw/DefaultDrawStrategy.java
+++ smartopencv/src/main/java/tech/huqi/smartopencv/draw/DefaultDrawStrategy.java
@@ -17,14 +17,37 @@ public class DefaultDrawStrategy implements IDrawStrategy {
 
     @Override
     public void drawBitmap(Canvas canvas, Bitmap frameBitmap, int surfaceWidth, int surfaceHeight) {
-        if (frameBitmap.getWidth() < surfaceWidth || frameBitmap.getHeight() < surfaceHeight) {
+        int bitmapWidth = frameBitmap.getWidth();
+        int bitmapHeight = frameBitmap.getHeight();
+        int left = 0;
+        int top = 0;
+        if (bitmapWidth > surfaceWidth) {
+            left = (frameBitmap.getWidth() - surfaceWidth) / 2;
+        }
+        if (bitmapHeight > surfaceHeight) {
+            top = (frameBitmap.getHeight() - surfaceHeight) / 2;
+        }
+        /**
+         * 如果相机预览大小（宽或高或宽高）大于控件大小，则从预览上选取子区域展示到控件上，
+         * 选取规则是让预览尽可能的居中显示到控件上
+         */
+        if (bitmapWidth <= surfaceWidth && bitmapHeight > surfaceHeight) {
+            // 宽度拉伸
+            canvas.drawBitmap(frameBitmap, new Rect(0, top, frameBitmap.getWidth(), frameBitmap.getHeight() - top),
+                    new Rect(0, 0, surfaceWidth, surfaceHeight), new Paint());
+        } else if (bitmapHeight <= surfaceHeight && bitmapWidth > surfaceWidth) {
+            // 高度拉伸
+            canvas.drawBitmap(frameBitmap, new Rect(left, 0, frameBitmap.getWidth() - left, frameBitmap.getHeight()),
+                    new Rect(0, 0, surfaceWidth, surfaceHeight), new Paint());
+        } else if (bitmapWidth <= surfaceWidth && bitmapHeight <= surfaceHeight) {
+            // 宽高比缩放
             Bitmap newBitmap = CameraHelper.scaleImage(frameBitmap, surfaceWidth, surfaceHeight);
             canvas.drawBitmap(newBitmap, new Rect(0, 0, newBitmap.getWidth(), newBitmap.getHeight()),
                     new Rect(0, 0, newBitmap.getWidth(), newBitmap.getHeight()), new Paint());
             newBitmap.recycle();
         } else {
-            canvas.drawBitmap(frameBitmap, new Rect(0, 0, frameBitmap.getWidth(), frameBitmap.getHeight()),
-                    new Rect(0, 0, frameBitmap.getWidth(), frameBitmap.getHeight()), new Paint());
+            canvas.drawBitmap(frameBitmap, new Rect(left, top, frameBitmap.getWidth() - left, frameBitmap.getHeight() - top),
+                    new Rect(0, 0, surfaceWidth, surfaceHeight), new Paint());
         }
         if (logCount < MAX_LOG_COUNT) {
             Util.printDebugLog(" BitmapWidth:" + frameBitmap.getWidth() + " BitmapHeight:" + frameBitmap.getHeight() +
